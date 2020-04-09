a1c83934d204ceb052e28cdb5e7b86bedd7b9206
Rance Campbell <rancewcampbell@gmail.com>
2020-04-09 20:54:36 +0000

    implemented fetchMyIP which grabs ip address

---
+++ index.js
@@ -0,0 +1,13 @@
+const { fetchMyIP } = require('./iss');
+
+
+
+// fetchMyIP((err, ip) => {
+//   if (err) {
+//     console.log("It didn't work!" , err);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+  
+// });
